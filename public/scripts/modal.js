f76edcde2d5002ce283c4b2cd8c91ef00a60cbef
Victor Cavichioli <79488234+VictorCavichioli@users.noreply.github.com>
2021-07-19 16:12:09 -0300

    Add files via upload

---
+++ public/scripts/modal.js
@@ -0,0 +1,23 @@
+export default function Modal(){
+    const modalWrapper = document.querySelector('.modal-wrapper')
+    const cancelButton = document.querySelector('.button.cancel')
+
+    cancelButton.addEventListener("click", close)
+
+    function open(){
+        //funcionalidade de atribuir a classe de active na modal
+        modalWrapper.classList.add("active")
+
+
+    }
+    function close(){
+        //funcionalidade de retirar a classe de active na modal
+        modalWrapper.classList.remove("active")
+
+    }
+
+    return{
+        open,
+        close
+    }
+}
---
+++ src/controllers/RoomController.js
@@ -0,0 +1,64 @@
+const Database = require("../db/config")
+
+module.exports = {
+    async create(req, res){
+        const db = await Database()
+        const pass = req.body.password
+        let roomId
+        let isRoom = true
+
+        while(isRoom ){
+
+            // Gera o número da sala
+            for(var i = 0; i < 6; i++){
+                i == 0 ? roomId = Math.floor(Math.random() * 10).toString() :
+                roomId += Math.floor(Math.random() * 10).toString()
+
+            }
+
+            // verifica se esse numero ja existe
+            const roomsExistIds = await db.all(`SELECT id FROM rooms`)
+
+            isRoom = roomsExistIds.some(roomExistId => roomExistId === roomId)
+
+            if(! roomsExistIds){
+                // Inseri a sala no banco
+                await db.run(`INSERT INTO rooms(
+                    id,
+                    pass
+                ) VALUES (
+                    ${parseInt(roomId)},
+                    ${pass}
+                )`)
+            }
+        }
+
+        await db.close()
+
+        res.redirect(`/room/${roomId}`)
+
+    },
+
+    async open(req,res){
+        const db = await Database()
+
+        const roomId = req.params.room
+
+        const questions = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 0`)
+
+        const questionsRead = await db.all(`SELECT * FROM questions WHERE room = ${roomId} and read = 1`)
+
+
+        res.render("room", {roomId:roomId, questions: questions, questionsRead:questionsRead})
+
+    },
+
+    enter(req,res){
+        const roomId = req.body.roomId
+
+        res.redirect(`/room/${roomId}`)
+    }
+
+
+}
+
